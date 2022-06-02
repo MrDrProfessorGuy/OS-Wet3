92f8ba78213634aa1843c37bbdf5d39ffb68c10e
MrDrprofessorGuy <guycohen801@gmail.com>
2022-06-02 10:52:54 +0300

    initial

---
+++ cmake-build-debug/CMakeFiles/Wet.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Wet.dir/main.cpp.o"
+  "Wet"
+  "Wet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Wet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
